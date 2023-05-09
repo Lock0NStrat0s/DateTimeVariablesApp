e952b57f96855bd71224ebbe17d5613d65f502a0
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-09 00:00:59 -0600

    code complete.

--- DateTimeVariables/Program.cs
+++ DateTimeVariables/Program.cs
@@ -1,6 +1,12 @@
 ﻿
-DateTime today = DateTime.Now;
+using System.Globalization;
 
-Console.WriteLine(today.ToString(""));
+DateTime today = DateTime.UtcNow;
 
+//DateTime birthday = DateTime.Parse("5/15/1998");
 
+//DateTime birthday = DateTime.ParseExact("6/11/1998", "d/M/yyyy", CultureInfo.InvariantCulture);
+
+//Console.WriteLine(birthday.ToString());
+
+Console.WriteLine(today.ToString(format: "MMMM dd, yyyy hh:mm tt zzz"));
